f76890c75cbd18a35505b062ce090408376414e0
Dmitry Potapov <potapov.d@gmail.com>
2016-09-18 01:01:53 +0300

    style fix

--- src/ops/releasedir.cpp
+++ src/ops/releasedir.cpp
@@ -1,6 +1,6 @@
 #include <fuse_lowlevel.h>
 
-#include <fs.hpp>        // xynta::dir_listing
+#include <fs.hpp>           // xynta::dir_listing
 
 void xynta_releasedir(fuse_req_t, fuse_ino_t, struct fuse_file_info* fi) {
     delete reinterpret_cast<xynta::dir_listing*>(fi->fh);
